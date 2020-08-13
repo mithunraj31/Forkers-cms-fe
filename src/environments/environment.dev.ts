353e9e91c7122d0df6413119a6e6f4e18fc43bc6
Mithunraj <mbel001@mbel.co.jp>
2020-08-13 17:12:07 +0900

    environment url commit

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -11,6 +11,6 @@
 export const environment = {
     production: false,
     //host: "http://18.177.107.1:8010/proxy/api/"
-    host: "http://locolhost:8080/api/"
+    host: "http://18.177.107.1:8010/proxy/api/"
   };
   
